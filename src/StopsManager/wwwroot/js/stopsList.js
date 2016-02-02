fa9838025e398ef2d1d0ca0b2f2036751314b65f
antgit <iljinaa@gmailcom>
2016-02-02 21:42:21 +0200

    info windows

--- src/StopsManager/wwwroot/js/stopsList.js
+++ src/StopsManager/wwwroot/js/stopsList.js
@@ -1,19 +1,38 @@
 ﻿define("stopsList", ["jquery"], function($) {
     function init() {
+        var lastOpenedInfoWindow = null;
+
         //todo move google to dependencies
         var map = new google.maps.Map(document.getElementById("map"), {
             center: { lat: 40.78, lng: -73.95 },
             zoom:13
         });
 
+        //todo load stops in parts
         $.ajax({
             url: "/Stops/Get"
         }).done(function(data) {
-            data.forEach(function(stop) {
+            data.forEach(function (stop) {
+                var contentString = stop.name;
+
+                var infoWindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+
                 var marker = new google.maps.Marker({
                     position: {lat: stop.latitude, lng: stop.longitude},
                     map: map,
                     title: stop.name
+                });
+
+                marker.addListener("click", function () {
+                    if (lastOpenedInfoWindow) {
+                        lastOpenedInfoWindow.close();
+                    }
+
+                    infoWindow.open(map, marker);
+
+                    lastOpenedInfoWindow = infoWindow;
                 });
             });
         }).fail(function(data) {
