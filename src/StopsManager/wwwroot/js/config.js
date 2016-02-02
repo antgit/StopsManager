5de38f35e6071c4a0d783afe6c136cfda20266af
antgit <iljinaa@gmailcom>
2016-02-02 21:12:23 +0200

    jquery and markers

--- src/StopsManager/wwwroot/js/config.js
+++ src/StopsManager/wwwroot/js/config.js
@@ -1,6 +1,12 @@
 ﻿requirejs.config({
     baseUrl: "/js",
     paths: {
+        jquery: "../lib/jquery/dist/jquery.min",
         stopsList: "stopsList"
+    },
+    shim: {
+        jquery: {
+            exports: '$'
+        }
     }
 });
--- src/StopsManager/wwwroot/js/stopsList.js
+++ src/StopsManager/wwwroot/js/stopsList.js
@@ -1,8 +1,23 @@
-﻿define("stopsList", [], function() {
+﻿define("stopsList", ["jquery"], function($) {
     function init() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            center: { lat: -34.397, lng: 150.644 },
-            zoom: 8
+        //todo move google to dependencies
+        var map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: 40.78, lng: -73.95 },
+            zoom:13
+        });
+
+        $.ajax({
+            url: "/Stops/Get"
+        }).done(function(data) {
+            data.forEach(function(stop) {
+                var marker = new google.maps.Marker({
+                    position: {lat: stop.latitude, lng: stop.longitude},
+                    map: map,
+                    title: stop.name
+                });
+            });
+        }).fail(function(data) {
+            console.error("Can't load stops data");
         });
     }
 
